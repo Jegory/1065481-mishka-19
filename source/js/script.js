a8ec4cce847a15f93b1964fbf577c7d4aeebcdc9
Egor Nazarovich <elf-jegory@yandex.ru>
2020-05-09 03:24:11 +0500

    Модуль-5 Исправление ошибок

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,5 @@
- // бургер
- var navMain = document.querySelector('.main-nav');
+  // бургер
+  var navMain = document.querySelector('.main-nav');
   var navToggle = document.querySelector('.main-nav__toggle');
 
   navMain.classList.remove('main-nav--nojs');
@@ -13,3 +13,7 @@
       navMain.classList.remove('main-nav--opened');
     }
   });
+
+  // no js
+navMain.classList.remove('main-nav--opened');
+navMain.classList.add('main-nav--closed');
